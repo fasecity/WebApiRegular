b1f6cfdb0d8348a7b1d8f759f78372e43efc539f
Mo <mohamoud51@hotmail.com>
2017-07-13 16:16:14 -0400

    Add project files.

---
+++ WebApiRegular/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace WebApiRegular.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
